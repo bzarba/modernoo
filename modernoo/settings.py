706628d4850339f1f781691bdccdd2007b1438aa
bzarba <bzarbabzarba@gmail.com>
2024-07-30 01:40:03 +0100

    fix

--- modernoo/settings.py
+++ modernoo/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecre-km88#!xkex7b$3jkiupk9j^005g5xw_b@##y87t8erccdzx@35'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
